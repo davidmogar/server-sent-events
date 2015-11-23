a9400893a488a401e6e333a555378922ae230025
David Moreno García <david.mogar@gmail.com>
2015-11-23 09:01:52 +0100

    Add alerts messages and randomize time between alerts

--- server/server.php
+++ server/server.php
@@ -1,12 +1,21 @@
 <?php
 
-    define("SECONDS_BETWEEN_ALERTS", 10);
+    define("SECONDS_BETWEEN_ALERTS", rand(5, 15));
+
+    $alerts = array(
+        "Coldest Air of the Season Sweeping Through Central and Southern States",
+        "Belgium on 'high alert'",
+        "Multiple raids in Brussels as police seek ISIS terrorists",
+        "Syria fighters may be fueled by amphetamines",
+        "Alien invasion? Strange sightings in Hong Kong",
+        "Experts criticise WHO delay in sounding alarm over Ebola outbreak"
+    );
 
     date_default_timezone_set("Europe/Madrid");
     header("Content-Type: text/event-stream\n\n");
 
     while (true) {
-        sendAlert("Test alert");
+        sendAlert(rand(0, 5));
 
         ob_end_flush();
         flush();
