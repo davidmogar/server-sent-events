511026771ac127ee2019a8c5d20f41fae50040f2
David Moreno García <david.mogar@gmail.com>
2015-11-23 09:04:13 +0100

    Fix alert selection

--- server/server.php
+++ server/server.php
@@ -15,7 +15,7 @@
     header("Content-Type: text/event-stream\n\n");
 
     while (true) {
-        sendAlert(rand(0, 5));
+        sendAlert($array(rand(0, 5)));
 
         ob_end_flush();
         flush();
