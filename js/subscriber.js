9b8177dc8bce61ae8a4688c064aab1c621139497
David Moreno García <david.mogar@gmail.com>
2015-11-23 09:20:19 +0100

    Add new sources and more info
    I changed createEventSource function to receive a JSON with all the needed
    configuration.

--- js/subscriber.js
+++ js/subscriber.js
@@ -1,21 +1,42 @@
 var eventSources = [];
 
-(function() {
-    createEventSource(
-      '156.35.95.69/server-sent-events/server.php',
-      {},
-      notifyAlert
-    )
+$(function() {
+  createEventSource({
+    source: 'Police',
+    uri: 'http://156.35.95.69/server-sent-events/server/server.php',
+    options: {},
+    onMessageCallback: notifyAlert
+  });
+
+  createEventSource({
+    source: 'Firefighters',
+    uri: 'http://156.35.95.69/server-sent-events/server/server.php',
+    options: {},
+    onMessageCallback: notifyAlert
+  });
+
+  createEventSource({
+    source: 'Weather alerts',
+    uri: 'http://156.35.95.69/server-sent-events/server/server.php',
+    options: {},
+    onMessageCallback: notifyAlert
+  });
 });
 
-function createEventSource(uri, options, onMessageCallback) {
-  var eventSource = new EventSource(uri, options);
-  eventSource.onmessage = onMessageCallback();
+function createEventSource(conf) {
+  var eventSource = new EventSource(conf.uri, conf.options);
+  eventSource.onmessage = function(e) { conf.onMessageCallback(e, conf.source); };
   eventSources.push(eventSource);
 
   return eventSource;
 }
 
-function notifyAlert(event) {
-  console.log(event);
+function notifyAlert(e, source) {
+  console.log(e.data);
+  var alert = JSON.parse(e.data);
+  console.log(alert);
+  var date = new Date(alert.timestamp);
+  var alert = '<li class="alert">' + source + ': ' + alert.message +
+      ' on ' + date.toString() + '</li>';
+  $('section#alerts ul').append(alert);
 }
