b5a3969386e4ac0eba65656667999923f77be348
David Moreno García <david.mogar@gmail.com>
2015-11-23 21:31:22 +0100

    Add alerts desktop notifications

--- js/subscriber.js
+++ js/subscriber.js
@@ -1,7 +1,11 @@
 const MAX_SHOWED_ALERTS = 15;
+const NOTIFICATION_DURATION = 5000;
+
 var eventSources = {};
+var hiddenProperty = getHiddenProperty();
 
 $(function() {
+  Notification.requestPermission();
 
   $('nav li a').click(function(event) {
     event.preventDefault();
@@ -30,19 +34,57 @@ function createEventSource(conf) {
   return eventSource;
 }
 
+function getHiddenProperty() {
+  var prefixes = ['webkit','moz','ms','o'];
+  var property;
+
+  if ('hidden' in document) {
+    property = 'hidden';
+  } else {
+    for (var i = 0; i < prefixes.length; i++) {
+      if ((prefixes[i] + 'Hidden') in document) {
+        property = prefixes[i] + 'Hidden';
+        break;
+      }
+    }
+  }
+
+  return property;
+}
+
+function isPageHidden() {
+  return (!hiddenProperty)? false : document[hiddenProperty];
+}
+
 function notifyAlert(e) {
   var alert = JSON.parse(e.data);
   var date = new Date(alert.timestamp);
-  var alert = '<li class="alert ' + alert.alertLevel + '"><span class="source">' +
+  var li = '<li class="alert ' + alert.alertLevel + '"><span class="source">' +
       alert.source + '</span><span class="date">' +
       date.toString() + '</span><p>' + alert.message + '</p></li>';
-  $('#alerts ul').prepend(alert);
+  $('#alerts ul').prepend(li);
   $('#alerts li:gt(' + (MAX_SHOWED_ALERTS - 1) + ')').remove();
+
+  showNotification(alert);
 }
 
 function removeEventSource(sourceUri) {
   if (eventSources.hasOwnProperty(sourceUri)) {
     eventSources[sourceUri].close();
     delete eventSources[sourceUri];
+  }
+}
+
+function showNotification(alert) {
+  if ("Notification" in window && isPageHidden()) {
+    if (Notification.permission === "granted") {
+      var notification = new Notification(alert.source, {
+        body: alert.message,
+        icon: '../img/alert-' + alert.alertLevel + '.png',
+        vibrate: [200, 100, 200]
+      });
+
+      setTimeout(function() { notification.close(); }, NOTIFICATION_DURATION);
+    }
   }
 }
